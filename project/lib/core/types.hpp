0c782118305a6a0914bc2d31884f4aa1277da4b6
nobodyxu <nobodyxu@lolol.me>
2019-10-01 16:57:41 +1000

    Add project/lib/core/types.hpp: Basic type definitions

---
+++ project/lib/core/types.hpp
@@ -0,0 +1,9 @@
+#ifndef  __Wonderfull_project_core_types_HPP__
+# define __Wonderfull_project_core_types_HPP__
+
+namespace core {
+using pin_t = unsigned char;
+using digital_t = bool;
+}
+
+#endif
