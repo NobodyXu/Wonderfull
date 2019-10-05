3ee7880e77db82ec8b58e16e91d7362fafb20882
nobodyxu <nobodyxu@lolol.me>
2019-10-05 15:12:57 +1000

    Fix a copy-paste error in week8.

--- week8/InfaredObjectDetection.ino
+++ week8/InfaredObjectDetection.ino
@@ -51,8 +51,8 @@ int irDetect(int irLedPin, int irReceiverPin, long frequency)
 {
   tone(irLedPin, frequency, 8);              // IRLED 38 kHz for at least 1 ms
   delay(1);                                  // Wait 1 ms
-  int ir lRead(irReceiverPin);       // IR receiver -> ir variable
-  delay(1= digita);                                  // Down time before recheck
+  int ir = digitalRead(irReceiverPin);       // IR receiver -> ir variable
+  delay(1);                                  // Down time before recheck
   return ir;                                 // Return 1 no detect, 0 detect
 }  
 
