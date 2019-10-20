87edca169ad34196769eae47fcaa71252992f198
nobodyxu <nobodyxu@lolol.me>
2019-10-20 16:26:43 +1100

    Fix meta/integral_limit.hpp
    The compilation error is due to the `min` and `max` macro defined in
    Arduino.hpp.
    
    So, rename `integral_limit::min` to `integral_limit::minimum` and
    `integral_limit::min` to `integral_limit::minimum`.

--- project/lib/meta/integral_limit.hpp
+++ project/lib/meta/integral_limit.hpp
@@ -6,15 +6,15 @@
 namespace meta {
 template <class T>
 struct integral_limit {
-    static constexpr T min() noexcept {
-        if constexpr (is_signed<T>)
+    static constexpr T minimum() noexcept {
+        if constexpr (is_signed_v<T>)
             return 1 << (sizeof(T) * 8 - 1);
         else
             return 0;
     }
 
-    static constexpr T max() noexcept {
-        return min() - 1;
+    static constexpr T maximum() noexcept {
+        return minimum() - 1;
     }
 };
 } /* namespace meta */
