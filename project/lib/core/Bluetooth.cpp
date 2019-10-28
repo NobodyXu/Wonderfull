0f12162065ed03510380974418f34803b797b539
nobodyxu <nobodyxu@lolol.me>
2019-10-28 13:45:42 +1100

    Add core::Comm::Bluetooth.  - Setup inheritance from core::Comm::Serial.  - Define basic functionalities info.

---
+++ project/lib/core/Bluetooth.cpp
@@ -0,0 +1,36 @@
+#include "Bluetooth.hpp"
+#include "serial.hpp"
+
+namespace core::Comm::impl {
+void issue_command(Bluetooth &bt, const char *str) {
+    serial.println(str);
+    bt.print(str);
+
+    bt.flush();
+    delay(1000);
+
+    bt.forward(serial);
+    
+    serial.println();
+    serial.flush();
+}
+} /* namespace impl */
+
+namespace core::Comm {
+Bluetooth::Bluetooth(pin_t RxD, pin_t TxD, pin_t resetPin):
+    base_t{RxD, TxD}, reset{resetPin, digitalPin_t::Mode::output}
+{
+    reset.write(digitalPin_t::Level::high);
+    base_t::begin(9600);
+}
+
+void Bluetooth::info() {
+    using impl::issue_command;
+
+    issue_command(*this, "AT");
+    issue_command(*this, "AT+BAUD?");
+    issue_command(*this, "AT+PASS?");
+    issue_command(*this, "AT+ROLE?");
+    issue_command(*this, "AT+ADDR?");
+}
+} /* namespace core::Comm */
---
+++ project/lib/core/Bluetooth.hpp
@@ -0,0 +1,62 @@
+#ifndef  __Wonderfull_project_core_Comm_Bluetooth_HPP__
+# define __Wonderfull_project_core_Comm_Bluetooth_HPP__
+
+# include "digitalPin_t.hpp"
+# include "Serial.hpp"
+# include <SoftwareSerial.h>
+
+namespace core::Comm {
+/*!
+ * Bluetooth for XC4382.
+ *
+ * Please define this variable inside loop(), not as global variable.
+ */
+class Bluetooth: private Serial<SoftwareSerial> {
+    digitalPin_t reset;
+
+    using base_t = Serial<SoftwareSerial>;
+
+public:
+    /*!
+     * @ RxD This pin should be connected to TxD on the bluetooth module.
+     * @ TxD This pin should be connected to RxD on the bluetooth module.
+     * @ resetPin This pin should be connected to BRK on the bluetooth module,
+     *            which is used to reset bluetooth connection.
+     */
+    Bluetooth(pin_t RxD, pin_t TxD, pin_t resetPin);
+
+    Bluetooth() = delete;
+
+    Bluetooth(const Bluetooth&) = delete;
+    Bluetooth(Bluetooth&&) = delete;
+
+    Bluetooth& operator = (const Bluetooth&) = delete;
+    Bluetooth& operator = (Bluetooth&&) = delete;
+
+    /*!
+     * Only expose interface of base class if necessary
+     */
+    using base_t::is_available;
+    using base_t::wait_for_available;
+
+    using base_t::getChar_noblock;
+    using base_t::getChar;
+    using base_t::read;
+
+    using base_t::print;
+    using base_t::println;
+    using base_t::forward;
+
+    using base_t::flush;
+
+    /*!
+     * print information about the bluetooth to usb.
+     */
+    void info();
+
+    //void connect_to_slave(int shieldPairNumber);
+    //void connect_to_master(int shieldPairNumber);
+};
+} /* namespace core::Comm */
+
+#endif
